ab199d0f75e9aa9b48fc221df49b5d8c9c4a12b1
Gu5tavoGallardo <g_gallargo01@icloud.com>
2024-11-03 17:51:07 -0600

    Update of this file
    This one is now has the function that will create the process

--- Scheduler.h
+++ Scheduler.h
@@ -1,27 +1,41 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
+
 #include <iostream>
 #include <queue>
 
-class Process{
-    public:
+class Process {
+public:
     int id;
     int cycles;
 
-    Process(int id, int cycles);
-
+    // Constructor to initialize a Process with an ID and cycle count
+    Process(int id, int cycles) : id(id), cycles(cycles) {}
 };
 
-class Scheduler{
-    public:
+class Scheduler {
+public:
+    // Queues for SJF and FCFS scheduling
     std::queue<Process> sjfQueue;
     std::queue<Process> fcfsQueue;
 
-    void addProcess(Process process);
+    // Method to add a process to the appropriate queue based on cycle count
+    void addProcess(Process process) {
+        if (process.cycles <= 50) {
+            sjfQueue.push(process);
+            std::cout << "Process " << process.id << " added to SJF queue with " << process.cycles << " cycles.\n";
+        } else {
+            fcfsQueue.push(process);
+            std::cout << "Process " << process.id << " added to FCFS queue with " << process.cycles << " cycles.\n";
+        }
+    }
 
-    void run();
-
-    
+    // Method to execute the scheduling logic
+    void run() {
+        std::cout << "Running scheduler...\n";
+        // Add your scheduling logic here
+        // For example, alternate between sjfQueue and fcfsQueue
+    }
 };
 
-#endif
+#endif // SCHEDULER_H
